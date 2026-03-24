edfc8981aaadb2ae41bf94312a87c255d83d42f1
Jorge Hunter <jgehunter@gmail.com>
2026-03-24 23:36:35 +0100

    wire up public API exports

--- markoutlib/__init__.py
+++ markoutlib/__init__.py
@@ -1,3 +1,18 @@
 """Markout P&L analysis for Python."""
 
 __version__ = "0.1.0"
+
+from markoutlib._compute import compute
+from markoutlib._horizons import Horizon, HorizonSet, seconds, ticks, trades
+from markoutlib._result import MarkoutResult
+
+__all__ = [
+    "compute",
+    "seconds",
+    "trades",
+    "ticks",
+    "Horizon",
+    "HorizonSet",
+    "MarkoutResult",
+    "__version__",
+]
